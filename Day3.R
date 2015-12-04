f4d44f90f3cf0f56faea737e46f5e3599bfc45d6
Don Schoolmater <dschoolmaster@gmail.com>
2015-12-04 14:16:00 -0600

    Add Day3 Part2 and map for Robo-Santa

--- Day3.R
+++ Day3.R
@@ -1,5 +1,6 @@
 ####Day 3
 dirs<-readLines("~/Path/To/InputDay3.txt")
+###Part 1###
 #create mapping function
 z<-function(x){
   switch(x,
@@ -15,8 +16,28 @@ santa.map<-matrix(0,nchar(dirs)+1,2)
 for(i in 1:nchar(dirs))santa.map[i+1,]<-santa.map[i,]+z(substring(dirs,i,i))
 #count unique locations
 length(unique(paste(santa.map[,1],santa.map[,2],sep=',')))
+#plot santa's path
 pdf("~/Path/To/Day3Map.pdf")
 plot(santa.map[,1],santa.map[,2],type='l',xlab='Longitiude',ylab='Latitude')
 points(0,0,pch=16,cex=1,col='green')
 points(tail(santa.map,1),pch=16,col='red')
+dev.off()
+###Part 2###
+santa.map<-robo.map<-c(0,0)
+for(i in 1:nchar(dirs)){
+  if(i%%2==1)santa.map<-rbind(santa.map,tail(santa.map,1)+z(substring(dirs,i,i)))
+  if(i%%2==0)robo.map<-rbind(robo.map,tail(robo.map,1)+z(substring(dirs,i,i)))
+}
+#cobmine coords, combine paths and count unique members
+length(unique(c(paste(santa.map[,1],santa.map[,2],sep=','),
+paste(robo.map[,1],robo.map[,2],sep=','))))
+
+#plot santa's and robo-santa's path
+pdf("~/Path/To//Day3Map2.pdf")
+plot(santa.map[,1],santa.map[,2],type='l',xlab='Longitiude',ylab='Latitude',ylim=c(-80,80))
+lines(robo.map[,1],robo.map[,2],col='blue')
+points(0,0,pch=16,cex=1,col='green')
+points(tail(santa.map,1),pch=16,col='red')
+points(tail(robo.map,1),pch=16,col='red')
+legend(10,70,c('Santa','Robo-Santa'),col=c('black','blue'),lty=1,bty='n')
 dev.off()
