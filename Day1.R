b6e282c83d45408b8c64fbad15c4c8093e998eed
Don Schoolmater <dschoolmaster@gmail.com>
2015-12-04 09:35:17 -0600

    Add annotation

--- Day1.R
+++ Day1.R
@@ -1,6 +1,10 @@
 ####Day 1####
-z<-readLines("~/Documents/paren.txt")
+#read the lines from
+z<-readLines("Path/To/Input/File/paren.txt")
+#cut out all open parens
 opn<-strsplit(z,'\\(')
+#cut out all close parens
 cls<-strsplit(z,'\\)')
+#take the resulting output from each and count the number remaining and subtract them
 sum(nchar(cls[[1]][grep('\\(',cls[[1]])]))-sum(nchar(opn[[1]][grep('\\)',opn[[1]])]))
 
