c1dc73d8d37e77c9550efdd21f78d85abc92058e
Don Schoolmater <dschoolmaster@gmail.com>
2015-12-04 11:25:24 -0600

    Add Day3 plot mapping

--- Day3.R
+++ Day3.R
@@ -15,4 +15,8 @@ santa.map<-matrix(0,nchar(dirs)+1,2)
 for(i in 1:nchar(dirs))santa.map[i+1,]<-santa.map[i,]+z(substring(dirs,i,i))
 #count unique locations
 length(unique(paste(santa.map[,1],santa.map[,2],sep=',')))
-
+pdf("~/Path/To/Day3Map.pdf")
+plot(santa.map[,1],santa.map[,2],type='l',xlab='Longitiude',ylab='Latitude')
+points(0,0,pch=16,cex=1,col='green')
+points(tail(santa.map,1),pch=16,col='red')
+dev.off()
