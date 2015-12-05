753a55b694d4e1efc70da2d44fb483a0b88ef9d0
Don Schoolmater <dschoolmaster@gmail.com>
2015-12-04 21:54:38 -0600

    Add Day1.R Part2

--- Day1.R
+++ Day1.R
@@ -1,6 +1,6 @@
 ####Day 1####
 #read the lines from
-z<-readLines("Path/To/InputDay1.txt")
+z<-readLines("~/Documents/AdventOfCode/InputDay1.txt")
 #cut out all open parens
 opn<-strsplit(z,'\\(')
 #cut out all close parens
@@ -8,3 +8,16 @@ cls<-strsplit(z,'\\)')
 #take the resulting output from each and count the number remaining and subtract them
 sum(nchar(cls[[1]][grep('\\(',cls[[1]])]))-sum(nchar(opn[[1]][grep('\\)',opn[[1]])]))
 
+####part 2####
+flr<-0
+pos<-0
+up.down<-function(x){
+  switch(x,
+         "("=1,
+         ")"=-1
+  )}
+while(flr>=0){
+  pos<-pos+1
+  flr<-flr+up.down(substring(z,pos,pos))
+}
+pos
